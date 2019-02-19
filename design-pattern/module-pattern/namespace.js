97b877f75ae660bd3d5306e0f726cdc3313d2724
jinyounglee <hs25823@naver.com>
2019-02-20 01:52:34 +0900

    Object clone

--- design-pattern/module-pattern/namespace.js
+++ design-pattern/module-pattern/namespace.js
@@ -7,3 +7,44 @@ function func() {
     console.log(myname);
 }   
 func();
+
+
+var i, myArray = [];
+myArray.push({name: 'hello'});
+myArray.push({name: 'hello2'});
+
+
+for (i = myArray.length; i--;) {
+    console.log( myArray[i].name );
+}
+
+var j = myArray.length;
+while(j--) {
+    console.log( myArray[j].name );
+}
+
+var man = {
+    hands: 2, 
+    legs: 2, 
+    heads: 1
+}
+
+if( typeof Object.prototype.clone === 'undefined' ) {
+    Object.prototype.clone = function() {};
+}
+/**
+ * man 객체가 define 될때 prototyp의 clone method 역시 생성된다. 
+ * hasOwnProperty로 filter를 줘서 man이 가지고 있는것만 print 한다. 
+ */
+for( var i in man ) {
+    if( man.hasOwnProperty(i) ) {         // filter 
+        console.log( i, man[i]);
+    }
+}
+
+for( i in man ){
+    if( Object.prototype.hasOwnProperty.call(man, i) ) {
+        console.log(i, ":", man[i]);
+    }
+}
+
