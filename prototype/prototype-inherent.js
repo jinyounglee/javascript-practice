97251e75054812ae6e626751b9ddfb3620df1cb1
jinyounglee <hs25823@naver.com>
2019-02-26 11:47:22 +0900

    연습

--- prototype/prototype-inherent.js
+++ prototype/prototype-inherent.js
@@ -24,9 +24,35 @@ function Customer(firstName, lastName, phone, membership) {
 Customer.prototype = Object.create(Person.prototype);
 
 // Make customer.prototype return Customer()
+// 하지 않으면 Person Constructor로 생성 한다. 
 Customer.prototype.constructor = Customer;
 
 // Create Customer 
 const customer1 = new Customer('Tom', 'Smith', '555-555-5555', 'Standard');
 console.log( customer1);
 console.log(customer1.greeting())
+
+function Product(price, qunatity) {
+    this.price = price;
+    this.qunatity = qunatity;
+    this.showDescription = function() {
+        return `Price : ${this.price}, quantity : ${this.qunatity}`;
+    }
+}
+
+const namecard = new Product(3000, 10000);
+
+console.log( namecard.showDescription() );
+
+function Namecard2 (price, quantity, tag) {
+    Product.call(this, price, quantity);
+    this.tag = tag;
+};
+
+Namecard2.prototype = Object.create(Product.prototype);
+Namecard2.prototype.constructor = Product;
+const newNameCard = new Namecard2(3000, 20000, 'TAG');
+
+// newNameCard.constructor = Namecard2;
+
+console.log( newNameCard.tag ); 
